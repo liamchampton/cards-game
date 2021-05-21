f5e6c768f86590f7f3883409712b5c2ad671e249
liamchampton <liam.hampton@hotmail.co.uk>
2021-05-21 17:41:37 +0100

    add test for saving and reading a deck from file

--- deck_test.go
+++ deck_test.go
@@ -1,9 +1,12 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
-	// create new deck
+	// create new deck and assign variables
 	deck := newDeck()
 	firstCard := deck[0]
 	lastCard := deck[len(deck)-1]
@@ -22,4 +25,19 @@ func TestNewDeck(t *testing.T) {
 	if lastCard != "Four of Clubs" {
 		t.Errorf("Expected last card to be Four of Clubs, but got %v", lastCard)
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
